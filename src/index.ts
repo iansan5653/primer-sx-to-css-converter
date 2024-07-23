96abad5a5082f780a059f49aa998723a91b5c5b1
Ian Sanders <iansan5653@gmail.com>
2024-07-23 15:05:39 +0000

    Add `Node` class and parse input

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,44 @@ import * as ts from "typescript";
 const inputElement = document.getElementById("input") as HTMLTextAreaElement;
 const outputElement = document.getElementById("output") as HTMLOutputElement;
 
-inputElement.addEventListener("input", () => {
-  const inputTs = inputElement.value;
+/** Wrapper around TS API. */
+class Node {
+  constructor(readonly sourceNode: ts.Node) {}
 
-  const sourceFile = ts.createSourceFile(
-    "input.ts",
-    inputTs,
-    ts.ScriptTarget.ESNext
+  get children() {
+    const result: Node[] = [];
+
+    ts.forEachChild(this.sourceNode, (child) => {
+      result.push(new Node(child));
+    });
+
+    return result;
+  }
+
+  get kind() {
+    return this.sourceNode.kind;
+  }
+
+  debug() {
+    const kind = ts.SyntaxKind[this.sourceNode.kind];
+    console.groupCollapsed(kind);
+    for (const child of this.children) child.debug();
+    console.groupEnd();
+  }
+}
+
+inputElement.addEventListener("input", () => {
+  const inputTs = `const x = ${inputElement.value}`;
+
+  const sourceFile = new Node(
+    ts.createSourceFile("input.ts", inputTs, ts.ScriptTarget.ESNext)
   );
 
-  const printer = ts.createPrinter();
+  const objectExpression =
+    sourceFile.children[0]?.children[0]?.children[0]?.children[1];
 
-  outputElement.textContent = printer.printFile(sourceFile);
+  if (objectExpression?.kind !== ts.SyntaxKind.ObjectLiteralExpression) {
+    outputElement.textContent = "Error: Input must be an object";
+    return;
+  }
 });
