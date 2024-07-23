87abd18777153e607942673589d1e8c83b41b98e
Ian Sanders <iansan5653@gmail.com>
2024-07-23 15:28:48 +0000

    Add ability to extract property names

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,17 @@ const inputElement = document.getElementById("input") as HTMLTextAreaElement;
 const outputElement = document.getElementById("output") as HTMLOutputElement;
 
 /** Wrapper around TS API. */
-class Node {
+class ChildWalker {
   constructor(readonly sourceNode: ts.Node) {}
 
   get children() {
-    const result: Node[] = [];
+    const result: ChildWalker[] = [];
 
     ts.forEachChild(this.sourceNode, (child) => {
-      result.push(new Node(child));
+      result.push(new ChildWalker(child));
     });
 
     return result;
-  }
-
-  get kind() {
-    return this.sourceNode.kind;
   }
 
   debug() {
@@ -29,18 +25,36 @@ class Node {
   }
 }
 
+function getPropertyName({name}: ts.ObjectLiteralElementLike) {
+  if (name && (ts.isLiteralExpression(name) || ts.isIdentifier(name)))
+    return name.text;
+  return null;
+}
+
+function propertyToCSSRule(property: ts.ObjectLiteralElementLike) {
+  const name = getPropertyName(property);
+
+  if (!name) return `/* Unable to parse "${property.getFullText()}" */`;
+
+  return `${name}: unset;`;
+}
+
 inputElement.addEventListener("input", () => {
   const inputTs = `const x = ${inputElement.value}`;
 
-  const sourceFile = new Node(
-    ts.createSourceFile("input.ts", inputTs, ts.ScriptTarget.ESNext)
+  const sourceFile = new ChildWalker(
+    ts.createSourceFile("input.ts", inputTs, ts.ScriptTarget.ESNext, true)
   );
 
   const objectExpression =
-    sourceFile.children[0]?.children[0]?.children[0]?.children[1];
+    sourceFile.children[0]?.children[0]?.children[0]?.children[1]?.sourceNode;
 
-  if (objectExpression?.kind !== ts.SyntaxKind.ObjectLiteralExpression) {
+  if (!objectExpression || !ts.isObjectLiteralExpression(objectExpression)) {
     outputElement.textContent = "Error: Input must be an object";
     return;
   }
+
+  outputElement.innerHTML = objectExpression.properties
+    .map(propertyToCSSRule)
+    .join("<br />");
 });
