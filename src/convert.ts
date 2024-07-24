dc89ec9c4270029e30beb62ce4ba7ae7036cccdb
Ian Sanders <iansan5653@gmail.com>
2024-07-24 17:30:07 +0000

    Extract `convert` out for reuse

---
+++ src/convert.ts
@@ -0,0 +1,117 @@
+import * as ts from "typescript";
+
+import {expandValueShorthands} from "./value-shorthands";
+import {nameShorthands} from "./name-shorthands";
+import * as css from "./css";
+import * as tsUtils from "./ts-utils";
+
+function kebabCase(name: string) {
+  return name.replaceAll(/[A-Z]/g, (match) => `-${match.toLowerCase()}`);
+}
+
+function transformPropertyName(name: string) {
+  const kebab = kebabCase(name);
+  return nameShorthands[kebab] ?? [kebab];
+}
+
+const breakpointValues = [, "544px", "768px", "1012px", "1280px"];
+
+function responsiveValueToBreakpoint(
+  ruleName: string,
+  value: string,
+  index: number
+) {
+  // Default first value is plain rule:
+  if (index === 0) return new css.Rule(ruleName, value);
+
+  const breakpoint = breakpointValues[index];
+  if (!breakpoint)
+    throw new Error(
+      `Breakpoint out of bounds for "${value}" in "${ruleName}" rule`
+    );
+
+  return new css.Block(`@media screen and (min-width: ${breakpoint})`, [
+    new css.Rule(ruleName, value),
+  ]);
+}
+
+function arrayToResponsiveCSS(
+  ruleName: string,
+  array: ts.ArrayLiteralExpression
+) {
+  const responsiveValues = tsUtils.getArrayValues(array);
+
+  if (!responsiveValues)
+    throw new Error(`Unsupported responsive array expression`);
+
+  return responsiveValues.map((value, index) => {
+    try {
+      return responsiveValueToBreakpoint(ruleName, value, index);
+    } catch (e) {
+      return new css.Comment((e as Error).toString());
+    }
+  });
+}
+
+function* propertyToCSSRules(
+  property: ts.ObjectLiteralElementLike
+): Generator<css.Expression, undefined, undefined> {
+  try {
+    const name = tsUtils.getPropertyName(property);
+    if (!name) throw new Error(`Unsupported name expression`);
+
+    const value = tsUtils.getPropertyValueExpression(property);
+    if (!value) throw new Error(`Unsupported property expression`);
+
+    // One property can become multiple rules, ie in the case of `py`
+    for (const ruleName of transformPropertyName(name))
+      if (ts.isLiteralExpression(value))
+        yield new css.Rule(
+          ruleName,
+          expandValueShorthands(ruleName, value.text)
+        );
+      else if (ts.isObjectLiteralExpression(value))
+        yield new css.Block(
+          ruleName,
+          Array.from(propertiesToCSSRules(value.properties))
+        );
+      else if (ts.isArrayLiteralExpression(value))
+        yield* arrayToResponsiveCSS(ruleName, value);
+      else throw new Error(`Unsupported value expression`);
+  } catch (e) {
+    yield new css.Comment(
+      `Unable to transform \`${property.getFullText()}\`: ${
+        (e as Error).message
+      }`
+    );
+  }
+}
+
+function* propertiesToCSSRules(
+  properties: ts.NodeArray<ts.ObjectLiteralElementLike>
+) {
+  for (const property of properties) yield* propertyToCSSRules(property);
+}
+
+export function convert(input: string) {
+  // auto wrap in braces if the user pasted part of an object
+  const trimmed = input.trim();
+  const hasBraces = trimmed.startsWith("{") || trimmed.endsWith("}");
+
+  const inputTs = `const _ = ${hasBraces ? input : `{${input}}`}`;
+
+  const sourceFile = new tsUtils.ChildWalker(
+    ts.createSourceFile("input.ts", inputTs, ts.ScriptTarget.ESNext, true)
+  );
+
+  sourceFile.debug();
+
+  const objectExpression =
+    sourceFile.children[0]?.children[0]?.children[0]?.children[1]?.sourceNode;
+
+  if (!objectExpression || !ts.isObjectLiteralExpression(objectExpression)) {
+    throw new Error("Error: Input must be an object");
+  }
+
+  return propertiesToCSSRules(objectExpression.properties);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,127 +1,15 @@
-import * as ts from "typescript";
-
-import {expandValueShorthands} from "./value-shorthands";
-import {nameShorthands} from "./name-shorthands";
-import * as css from "./css";
-import * as tsUtils from "./ts-utils";
+import {convert} from "./convert";
 
 const inputElement = document.getElementById("input") as HTMLTextAreaElement;
 const outputElement = document.getElementById("output") as HTMLOutputElement;
 
-function kebabCase(name: string) {
-  return name.replaceAll(/[A-Z]/g, (match) => `-${match.toLowerCase()}`);
-}
-
-function transformPropertyName(name: string) {
-  const kebab = kebabCase(name);
-  return nameShorthands[kebab] ?? [kebab];
-}
-
-const breakpointValues = [, "544px", "768px", "1012px", "1280px"];
-
-function responsiveValueToBreakpoint(
-  ruleName: string,
-  value: string,
-  index: number
-) {
-  // Default first value is plain rule:
-  if (index === 0) return new css.Rule(ruleName, value);
-
-  const breakpoint = breakpointValues[index];
-  if (!breakpoint)
-    throw new Error(
-      `Breakpoint out of bounds for "${value}" in "${ruleName}" rule`
-    );
-
-  return new css.Block(`@media screen and (min-width: ${breakpoint})`, [
-    new css.Rule(ruleName, value),
-  ]);
-}
-
-function arrayToResponsiveCSS(
-  ruleName: string,
-  array: ts.ArrayLiteralExpression
-) {
-  const responsiveValues = tsUtils.getArrayValues(array);
-
-  if (!responsiveValues)
-    throw new Error(`Unsupported responsive array expression`);
-
-  return responsiveValues.map((value, index) => {
-    try {
-      return responsiveValueToBreakpoint(ruleName, value, index);
-    } catch (e) {
-      return new css.Comment((e as Error).toString());
-    }
-  });
-}
-
-function* propertyToCSSRules(
-  property: ts.ObjectLiteralElementLike
-): Generator<css.Expression, undefined, undefined> {
-  try {
-    const name = tsUtils.getPropertyName(property);
-    if (!name) throw new Error(`Unsupported name expression`);
-
-    const value = tsUtils.getPropertyValueExpression(property);
-    if (!value) throw new Error(`Unsupported property expression`);
-
-    // One property can become multiple rules, ie in the case of `py`
-    for (const ruleName of transformPropertyName(name))
-      if (ts.isLiteralExpression(value))
-        yield new css.Rule(
-          ruleName,
-          expandValueShorthands(ruleName, value.text)
-        );
-      else if (ts.isObjectLiteralExpression(value))
-        yield new css.Block(
-          ruleName,
-          Array.from(propertiesToCSSRules(value.properties))
-        );
-      else if (ts.isArrayLiteralExpression(value))
-        yield* arrayToResponsiveCSS(ruleName, value);
-      else throw new Error(`Unsupported value expression`);
-  } catch (e) {
-    yield new css.Comment(
-      `Unable to transform \`${property.getFullText()}\`: ${
-        (e as Error).message
-      }`
-    );
-  }
-}
-
-function* propertiesToCSSRules(
-  properties: ts.NodeArray<ts.ObjectLiteralElementLike>
-) {
-  for (const property of properties) yield* propertyToCSSRules(property);
-}
-
 inputElement.addEventListener("input", () => {
-  let input = inputElement.value;
-
-  // auto wrap in braces if the user pasted part of an object
-  const trimmed = input.trim();
-  const hasBraces = trimmed.startsWith("{") || trimmed.endsWith("}");
-  if (!hasBraces) input = `{${input}}`;
-
-  const inputTs = `const _ = ${input}`;
-
-  const sourceFile = new tsUtils.ChildWalker(
-    ts.createSourceFile("input.ts", inputTs, ts.ScriptTarget.ESNext, true)
-  );
-
-  sourceFile.debug();
-
-  const objectExpression =
-    sourceFile.children[0]?.children[0]?.children[0]?.children[1]?.sourceNode;
-
-  if (!objectExpression || !ts.isObjectLiteralExpression(objectExpression)) {
-    outputElement.textContent = "Error: Input must be an object";
-    return;
+  try {
+    const cssProperties = convert(inputElement.value);
+    outputElement.textContent = Array.from(cssProperties).join("\n");
+  } catch (e) {
+    outputElement.textContent = (e as Error).toString();
   }
-  outputElement.textContent = Array.from(
-    propertiesToCSSRules(objectExpression.properties)
-  ).join("\n");
 });
 
 inputElement.addEventListener("focus", () => {
